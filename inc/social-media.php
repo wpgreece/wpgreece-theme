ef2d68c7528b765cfd3bb64ae2d4ccce5ce5ad8b
Iakovos Frountas <iakovosfrountas@gmail.com>
2017-01-12 13:24:04 +0200

    Add Speakerdeck and YouTube icons

--- inc/social-media.php
+++ inc/social-media.php
@@ -1,7 +1,7 @@
  <p class = "footer-social">
     <a href = "<?php echo get_field( 'facebook_page', 'option' ); ?>" title = "Facebook page"><span class="facebook"></span></a>
     <a href = "<?php echo get_field( 'twitter_page', 'option' ); ?>" title = "Twitter page"><span class="twitter"></span></a>
-    <a href = "<?php echo get_field( 'linkedin_page', 'option' ); ?>" title = "linkedin page"><span class="linkedin"></span></a>
+    <a href = "<?php echo get_field( 'speakerdeck', 'option' ); ?>" title = "Speaker Deck"><span class="speakerdeck"></span></a>
     <a href = "<?php echo get_field( 'instagram_page', 'option' ); ?>" title = "Instagram page"><span class="instagram"></span></a>
-    <a href = "<?php echo get_field( 'meetup_page', 'option' ); ?>" title = "LinkedIn page"><span class="meetup"></span></a>
+    <a href = "<?php echo get_field( 'youtube', 'option' ); ?>" title = "Youtube Channel"><span class="youtube"></span></a>
 </p>

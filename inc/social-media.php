d3ed377fda0fe9abba14786ca4c2c6d674c3a896
Iakovos Frountas <iakovosfrountas@gmail.com>
2017-01-12 13:31:27 +0200

    Add target blank in social media icons

--- inc/social-media.php
+++ inc/social-media.php
@@ -1,7 +1,7 @@
  <p class = "footer-social">
-    <a href = "<?php echo get_field( 'facebook_page', 'option' ); ?>" title = "Facebook page"><span class="facebook"></span></a>
-    <a href = "<?php echo get_field( 'twitter_page', 'option' ); ?>" title = "Twitter page"><span class="twitter"></span></a>
-    <a href = "<?php echo get_field( 'speakerdeck', 'option' ); ?>" title = "Speaker Deck"><span class="speakerdeck"></span></a>
-    <a href = "<?php echo get_field( 'instagram_page', 'option' ); ?>" title = "Instagram page"><span class="instagram"></span></a>
-    <a href = "<?php echo get_field( 'youtube', 'option' ); ?>" title = "Youtube Channel"><span class="youtube"></span></a>
+    <a href = "<?php echo get_field( 'facebook_page', 'option' ); ?>" target="_blank" title = "Facebook page"><span class="facebook"></span></a>
+    <a href = "<?php echo get_field( 'twitter_page', 'option' ); ?>" target="_blank" title = "Twitter page"><span class="twitter"></span></a>
+    <a href = "<?php echo get_field( 'speakerdeck', 'option' ); ?>" target="_blank" title = "Speaker Deck"><span class="speakerdeck"></span></a>
+    <a href = "<?php echo get_field( 'instagram_page', 'option' ); ?>" target="_blank" title = "Instagram page"><span class="instagram"></span></a>
+    <a href = "<?php echo get_field( 'youtube', 'option' ); ?>" target="_blank" title = "Youtube Channel"><span class="youtube"></span></a>
 </p>

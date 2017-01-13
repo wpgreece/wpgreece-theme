99cdc4949c7f8f786a1bb48ecc90c01a6cfd3120
Kostas Vrouvas <kosvrouvas@gmail.com>
2017-01-14 01:39:26 +0200

    fixes

--- inc/featured-article.php
+++ inc/featured-article.php
@@ -32,7 +32,7 @@
 
         <div class="author-image">
 
-            <?php echo get_avatar( get_the_author_meta('user_email') , 130 ); ?>
+            <?php echo get_avatar( get_the_author_meta('user_email') , 90 ); ?>
 
         </div>
 
@@ -105,7 +105,7 @@
 
                     <div class="column-20">
 
-                        <?php echo get_avatar( get_the_author_meta('user_email') , 90 ); ?>
+                        <?php echo get_avatar( get_the_author_meta('user_email') , 130 ); ?>
 
                     </div>
 

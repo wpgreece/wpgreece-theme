473e836b5998edc9585e501f9343833d5a9b0458
Iakovos Frountas <iakovosfrountas@gmail.com>
2017-01-13 23:15:24 +0200

    Built related articles

---
+++ inc/related-articles.php
@@ -0,0 +1,8 @@
+     <!-- RELATED ARTICLES -->
+     <?php (if is_single( ) ) : ?>
+                        
+        <div class="more-news column">
+            <h2><?php _e('Σχτεικά Άρθρα', 'wpgc'); ?> </h2>
+        </div>
+
+    <?php endif; ?>

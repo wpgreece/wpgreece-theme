47b03cc8851e5195b6cd274e62db2d09044d4200
Iakovos Frountas <iakovosfrountas@gmail.com>
2017-01-14 01:15:51 +0200

    Rollback

--- inc/featured-article.php
+++ inc/featured-article.php
@@ -114,8 +114,7 @@
                         <div class="author-name-details">
 
                             <h3 class="author-name"> <?php echo get_author_name( ); ?> </h3>
-                            <?php $id = get_current_user_id(); ?>
-                            <a href="<?php  echo get_the_author_meta('facebook', $id ); ?>" class="facebook" title="Facebook"><img src = "<?php bloginfo ( 'template_url' ) ?>/img/social/facebook.png" alt = "Βρες με στα Social" /></a>
+                            <a href=""><img src = "<?php bloginfo ( 'template_url' ) ?>/img/social/facebook.png" alt = "Βρες με στα Social" /></a>
                             <a href=""><img src = "<?php bloginfo ( 'template_url' ) ?>/img/social/twitter.png" alt = "Βρες με στα Social" /></a>
 
                         </div>

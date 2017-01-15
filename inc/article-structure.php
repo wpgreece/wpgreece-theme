8eceb6aad2d1a9df29c69c39ee50bd34abb516f5
Iakovos Frountas <iakovosfrountas@gmail.com>
2017-01-15 03:40:50 +0200

    add medium image size

--- inc/article-structure.php
+++ inc/article-structure.php
@@ -7,11 +7,11 @@
 
         <?php 
             $image_acf = get_field('default_featured_image', 'option');
-            $size_img_acf = 'front-page-rectangle';
+            $size_img_acf = 'medium';
             $link_image_acf = vanilla_theme_get_image_src( $image_acf, $size_img_acf ); ?>
 
         <img src = "<?php echo $link_image_acf; ?>" alt = "<?php the_title(); ?>" />
-        
+
     <?php endif; ?>
 
 <?php get_template_part( 'inc/primary' ); ?>

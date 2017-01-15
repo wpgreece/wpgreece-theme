2ec87ea4b271f49bfdeea068fcad00867e8b9294
Kostas Vrouvas <kosvrouvas@gmail.com>
2017-01-15 22:23:20 +0200

    fix title frontpage

--- inc/featured-article.php
+++ inc/featured-article.php
@@ -43,7 +43,7 @@
 
             <a href = "<?php the_permalink(); ?>" title = "<?php the_title(); ?>">
 
-                <h3><?php the_title(); ?></h3>
+                <h3 class="featured-article-title"><?php the_title(); ?></h3>
 
                 <?php the_excerpt();?>
 

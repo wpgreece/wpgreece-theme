8f8da6c10d756ac0ec345850aebeeb957e204e13
Iakovos Frountas <iakovosfrountas@gmail.com>
2017-01-24 10:57:43 +0200

    Fix 404 page

--- 404.php
+++ 404.php
@@ -1,10 +1,8 @@
 <?php get_header(); ?>
 
-
 <section class = "content row page-container responsiville-equalheights">
 
         <article class = "column tablet-column-100 laptop-column-65 laptop-push-15">
-
             
             <!-- ERROR 404 TITLES AND STUFF -->
 
@@ -13,17 +11,13 @@
             <h2>
                 <?php _e( 'Η σελίδα που ζητήσατε είτε έχει διαγραφεί είτε δεν υπάρχει.', 'wpgc' ); ?>
             </h2>
-
             
-
         </article>
 
     <?php include('inc/left-sidebar.php'); ?>
     <?php include('inc/right-sidebar.php'); ?>
 
 
-</section> <!-- .content -->
-   
-
+</section> <!-- .content --> 
 
 <?php get_footer(); ?>

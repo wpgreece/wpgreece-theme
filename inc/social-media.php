79f119ad6d5894723f6f806167a6d393d203e93c
Kostas Vrouvas <kosvrouvas@codesigns.gr>
2018-05-23 18:02:41 +0300

    Fix issue #12

--- inc/social-media.php
+++ inc/social-media.php
@@ -1,5 +1,5 @@
  <p class = "footer-social">
-    <a href = "<?php echo get_field( 'facebook_page', 'option' ); ?>" target="_blank" title = "Facebook page"><span class="facebook"></span></a>
+    <a href = "<?php echo get_field( 'facebook_page', 'option' ); ?>" target="_blank" title = "Facebook group"><span class="facebook"></span></a>
     <a href = "<?php echo get_field( 'twitter_page', 'option' ); ?>" target="_blank" title = "Twitter page"><span class="twitter"></span></a>
     <a href = "<?php echo get_field( 'speakerdeck', 'option' ); ?>" target="_blank" title = "Speaker Deck"><span class="speakerdeck"></span></a>
     <a href = "<?php echo get_field( 'instagram_page', 'option' ); ?>" target="_blank" title = "Instagram page"><span class="instagram"></span></a>

198b87bba808e5a38f519ecb77bed6be7e2b75fa
Kostas Vrouvas <hello@kosvrouvas.com>
2021-10-13 10:42:12 +0300

    Add `rel="noopener"`

--- inc/social-media.php
+++ inc/social-media.php
@@ -1,8 +1,8 @@
  <p class = "footer-social">
-    <a href = "<?php echo get_field( 'facebook_page', 'option' ); ?>" target="_blank" title = "Facebook group"><span class="facebook"></span></a>
-    <a href = "<?php echo get_field( 'twitter_page', 'option' ); ?>" target="_blank" title = "Twitter page"><span class="twitter"></span></a>
-    <a href = "<?php echo get_field( 'speakerdeck', 'option' ); ?>" target="_blank" title = "Speaker Deck"><span class="speakerdeck"></span></a>
-    <a href = "<?php echo get_field( 'instagram_page', 'option' ); ?>" target="_blank" title = "Instagram page"><span class="instagram"></span></a>
-    <a href = "<?php echo get_field( 'youtube', 'option' ); ?>" target="_blank" title = "Youtube Channel"><span class="youtube"></span></a>
+    <a href = "<?php echo get_field( 'facebook_page', 'option' ); ?>" target="_blank" title = "Facebook group" rel="noopener"><span class="facebook"></span></a>
+    <a href = "<?php echo get_field( 'twitter_page', 'option' ); ?>" target="_blank" title = "Twitter page" rel="noopener"><span class="twitter"></span></a>
+    <a href = "<?php echo get_field( 'speakerdeck', 'option' ); ?>" target="_blank" title = "Speaker Deck" rel="noopener"><span class="speakerdeck"></span></a>
+    <a href = "<?php echo get_field( 'instagram_page', 'option' ); ?>" target="_blank" title = "Instagram page" rel="noopener"><span class="instagram"></span></a>
+    <a href = "<?php echo get_field( 'youtube', 'option' ); ?>" target="_blank" title = "Youtube Channel" rel="noopener"><span class="youtube"></span></a>
     <!-- <a href = "<?php echo get_field( 'flickr_page', 'option' ); ?>" target="_blank" title = "Flickr"><span class="flickr2"></span></a> -->
 </p>
